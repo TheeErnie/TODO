8529ab633391f777857f0040cfa024fd438a67f1
TheeErnie <wdoactual@gmail.com>
2023-10-29 20:40:17 -0500

    10/29 - autofill day's event panel with blank event objects removed horizontal scroll bars. Began implementation of School Event subclass.

---
+++ TDESchool.cs
@@ -0,0 +1,24 @@
+﻿namespace TODO
+{
+    public enum TDDayofWeek
+    {
+        Sunday = 0, Monday = 1, Tuesday = 2, Wednesday = 3, Thursday = 4, Friday = 5, Saturday = 6
+    }
+    public class TDESchool : TDEvent
+    {
+        //attributes
+        private string _course_name;
+        private string _course_id;
+        private bool _online;
+        private List<TDDayofWeek> _meeting_days;
+
+        //constructors
+        public TDESchool() : base()
+        {
+            _course_name = "Unknown";
+            _course_id = "ABC-123";
+            _online = false;
+
+        }
+    }
+}
---
+++ TDEvent.cs
@@ -0,0 +1,60 @@
+﻿namespace TODO
+{
+    public class TDEvent
+    {
+        //attributes
+        private DateTime _start_time;
+        private DateTime _end_time;
+        private string _name;
+        private string _description;
+        private bool _completed;
+
+        //constructor
+        public TDEvent()
+        {
+            _start_time = DateTime.Today; 
+            _end_time = DateTime.Today;
+            _name = "NO_NAME_GIVEN";
+            _description = string.Empty;
+            _completed = false;
+        }
+
+        public TDEvent(DateTime start_time, DateTime end_time, string name, string description)
+        {
+            _start_time=start_time;
+            _end_time=end_time;
+            _name=name;
+            _description=description;
+            _completed = false;
+        }
+
+        //properties
+        public string StartTimeString
+        {
+            get { return _start_time.ToString("h:mm tt"); }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        //methods
+        public void UpdateAvailability()
+        {
+
+        }
+
+        public void NotifyUser()
+        {
+
+        }
+
+        public void VerifyEventCompletion()
+        {
+            bool ui = false;
+            //userinput
+            _completed = ui;
+        }
+    }
+}
